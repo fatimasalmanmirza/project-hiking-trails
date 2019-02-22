0005ec71ed5369eafd4b9d5d3bffcb5fcce90653
vagrant <vagrant@vagrant.vm>
2019-02-22 08:27:17 +0000

    function of sending a scheduled msg to user by cron

--- send_sms.py
+++ send_sms.py
@@ -1,29 +1,65 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
+from model import connect_to_db, db, User
+from flask import Flask
+from server import get_trail
 import os
- 
-# Your Account Sid and Auth Token from twilio.com/console
-account_sid = os.environ["account_sid"]
-auth_token = os.environ["auth_token"]
-client = Client(account_sid, auth_token)
-
-message = client.messages \
-                .create(
-                     body="Hi, here is our recommendation of Hike trails for your weekend",
-                     from_='+13347317307',
-                     to='+14159106112'
-                 )
-
-print(message.sid)
+# import schedule
+# import time
 
 
-# POST https://api.twilio.com/2010-04-01/Accounts/{TestAccountSid}/Messages
 
-# +13347317307
 
-# Account Sid
 
-# AC6ee6806642a619b8e5e11983cf52b26f
 
-# Auth Token
-# cc391ab8ef49943b9bcc61d0bb6431d2
+def send_msg():
+	"""Sending trail recommendation every week."""
+
+
+	account_sid = os.environ["account_sid"]
+	auth_token = os.environ["auth_token"]
+	users = User.query.all()
+	for user in users:
+		if user.phone_number is None:
+			return "phone number does not exist {}".format(user.user_id)
+		if user.location is None:
+			return "location not found {}".format(user.user_id)
+
+		user_phonenumber = user.phone_number
+		user_location = user.location
+
+	 
+		# Your Account Sid and Auth Token from twilio.com/console
+		test_sid = 'ACa07eecf8707d51cbb03d2353c182a433'
+		test_auth_token = '0e013261e0966370cfa1d5eb47cc2aab'
+
+		test = False
+
+		if test:
+			client = Client(test_sid, test_auth_token)
+		else:
+			client = Client(account_sid, auth_token)
+
+		message = client.messages \
+		                .create(
+		                    	body="Hi Hike every weekend recommends you this trail for your weekend\n"+get_trail(user_location),
+		                    	from_='+13347317307',
+		                    	to=user_phonenumber
+		                 )
+
+		if message.sid is not None:
+			print(f'sms sent to {user_phonenumber}: {message.sid}')
+
+	return 0
+
+if __name__ == "__main__":
+
+    app = Flask(__name__)
+
+    app.debug = True
+
+    connect_to_db(app)
+    send_msg()
+
+
+    # DebugToolbarExtension(app)###############################################################################
