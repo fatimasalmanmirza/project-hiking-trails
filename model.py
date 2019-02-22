f8a34acd46780c71e1cc7537332abae30878a7e0
vagrant <vagrant@vagrant.vm>
2019-02-22 08:29:41 +0000

    spacing done

--- model.py
+++ model.py
@@ -29,30 +29,7 @@ class User(db.Model):
 			self.location, self.is_parking, self.is_restrooms, self.is_dogfriendly,
 			self.is_kidsfriendly, self.is_daily, self.is_weekly, self.is_monthly)
 
-# class Preference(db.Model):
-# 	"""preferences of users"""
 
-# 	__tablename__ = 'preferences'
-
-# 	user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-# #	preference_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-# 	location = db.Column(db.String(100), nullable=False)
-# 	is_parking = db.Column(db.Boolean, nullable=True)
-# 	is_restrooms = db.Column(db.Boolean, nullable=True)
-# 	is_dogfriendly = db.Column(db.Boolean, nullable=True)
-# 	is_kidsfriendly = db.Column(db.Boolean, nullable=True)
-# 	is_daily = db.Column(db.Boolean, nullable=True)
-# 	is_weekly = db.Column(db.Boolean, nullable=True)
-# 	is_monthly = db.Column(db.Boolean, nullable=True)
-
-# 	user = db.relationship("User",backref='preferences') #need to see 
-# 	#what should i do in backref if there is one to one relationship
-
-	# def __repr__(self):
-
-	# 	repr_str = "<Preference:  {}, {}, {}, {}, {}, {}, {}, {} >"
-	# 	return repr_str.format(self.location, self.is_parking, self.is_restrooms, self.is_dogfriendly,
-	# 		self.is_kidsfriendly, self.is_daily, self.is_weekly, self.is_monthly)
 							
 def init_app():
 	# So that we can use Flask-SQLAlchemy, we'll make a Flask app.
